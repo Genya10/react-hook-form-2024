5b522e0a80d3f82d83f4fbbc484bb9988b38c6d9
Jenya <113167825+Genya10@users.noreply.github.com>
2024-06-24 17:14:26 +0300

    add type and use formData

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,52 @@
-import {useForm} from 'react-hook-form'
-import './App.scss'
+import { SubmitHandler, useForm } from "react-hook-form";
+import "./App.scss";
+import { IForm } from "./types/form";
 
 function App() {
-
-  const {register, handleSubmit} = useForm({
+  const { register, handleSubmit, formState } = useForm<IForm>({
     //ОПЦИИ ФОРМЫ
-    defaultValues:{'e-mail':'test@com.ua'},
-    mode:'onChange',
-  })
+    //defaultValues:{'email':'test@com.ua'},
+    mode: "onChange",
+  });
 
-//eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const onSubmit = (data:any) =>{
-    console.log(data)
-  }
+  const emailError = formState.errors["email"]?.message;
+
+  const onSubm: SubmitHandler<IForm> = (data) => {
+    console.log(data["email"]);
+  };
 
   return (
     <>
       <h1>Feedback form</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubm)}>
         <input
-           type="text" 
-            placeholder='Enter e-mail:'
-            {...register('e-mail', {
-              required:'This field is required',
-            })}
-            />
-
-          <textarea  placeholder='Enter message:'></textarea>
-          <button type='submit'>Send</button>
+          type="text"
+          placeholder="Enter email:"
+          {...register("email", {
+            required: "This field is required",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Invalid email address",
+            },
+          })}
+        />
+        {emailError && (
+          <p
+            style={{
+              color: "red",
+              margin: "2px auto",
+              textAlign: "left",
+            }}
+          >
+            {emailError}
+          </p>
+        )}
+        <textarea placeholder="Enter message:"></textarea>
+        <button type="submit">Send</button>
       </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
