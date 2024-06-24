ec737effa65eb1ae818b9a82e6106f0c29d68496
Jenya <113167825+Genya10@users.noreply.github.com>
2024-06-24 16:25:31 +0300

    add useForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-
+import {useForm} from 'react-hook-form'
 import './App.scss'
 
 function App() {
 
+  const {register, handleSubmit} = useForm({
+    //ОПЦИИ ФОРМЫ
+    defaultValues:{'e-mail':'test@com.ua'},
+    mode:'onChange',
+  })
+
+//eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSubmit = (data:any) =>{
+    console.log(data)
+  }
+
   return (
     <>
       <h1>Feedback form</h1>
-      <form>
-        <input type="text"  placeholder='Enter e-mail'/>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+           type="text" 
+            placeholder='Enter e-mail:'
+            {...register('e-mail', {
+              required:'This field is required',
+            })}
+            />
+
           <textarea  placeholder='Enter message:'></textarea>
           <button type='submit'>Send</button>
       </form>
